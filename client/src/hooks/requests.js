8a3a12965b26b34a56e01341b3ebf4a1b407a398
Gaurav <gauravpandey448@gmail.com>
2022-02-03 22:16:12 +0530

    first commit

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,51 @@
+const API_URL = 'http://localhost:8000/v1';
+
+async function httpGetPlanets() {
+  // TODO: Once API is ready.
+  const response = await fetch(`${API_URL}/planets`);
+  // Load planets and return as JSON.
+  return await response.json();
+}
+
+async function httpGetLaunches() {
+  // TODO: Once API is ready.
+  const response = await fetch(`${API_URL}/launches`);
+  // Load launches, sort by flight number, and return as JSON.
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
+}
+
+async function httpSubmitLaunch(launch) {
+  try {
+    // TODO: Once API is ready.
+    return await fetch(`${API_URL}/launches`, {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (err) {
+    return { ok: false };
+  }
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: 'delete',
+    });
+  } catch (err) {
+    console.log(err);
+    return {
+      ok: false,
+    };
+  }
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
